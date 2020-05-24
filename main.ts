2cdd5d6554ddf7567a8feae3259a4f443e661ac7
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-24 19:40:51 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,27 @@
 function macheEtwas () {
-    serial.writeValue("x", AnalogPin.P0)
-    serial.writeValue("x", AnalogPin.P1)
-    serial.writeValue("x", AnalogPin.P2)
-    serial.writeValue("x", AnalogPin.P3)
-    serial.writeValue("x", AnalogPin.P4)
-    serial.writeValue("x", AnalogPin.P5)
-    serial.writeValue("x", AnalogPin.P6)
-    serial.writeValue("x", AnalogPin.P7)
-    serial.writeValue("x", AnalogPin.P8)
-    serial.writeValue("x", AnalogPin.P9)
-    serial.writeValue("x", AnalogPin.P10)
-    serial.writeValue("x", AnalogPin.P11)
+    serial.writeValue("x0", AnalogPin.P0)
+    serial.writeValue("x1", AnalogPin.P1)
+    serial.writeValue("x2", AnalogPin.P2)
+    serial.writeValue("x3", AnalogPin.P3)
+    serial.writeValue("x4", AnalogPin.P4)
+    serial.writeValue("x5", AnalogPin.P5)
+    serial.writeValue("x6", AnalogPin.P6)
+    serial.writeValue("x7", AnalogPin.P7)
+    serial.writeValue("x8", AnalogPin.P8)
+    serial.writeValue("x9", AnalogPin.P9)
+    serial.writeValue("x10", AnalogPin.P10)
+    serial.writeValue("x11", AnalogPin.P11)
+    serial.writeValue("x12", AnalogPin.P12)
+    serial.writeValue("x13", AnalogPin.P13)
+    serial.writeValue("x14", AnalogPin.P14)
 }
 input.onButtonPressed(Button.A, function () {
     macheEtwas()
 })
+input.onButtonPressed(Button.B, function () {
+    ampel_nr = 4
+})
 function eine_seite (aNum: number) {
-    serial.writeValue("x", ampel_liste[aNum] + 2)
     pins.analogWritePin(ampel_liste[aNum]+2, 0)
 pins.analogWritePin(ampel_liste[aNum]+0, 512)
 basic.pause(2000)
@@ -35,8 +40,9 @@ pins.analogWritePin(ampel_liste[aNum]+1, 0)
 pins.analogWritePin(ampel_liste[aNum]+2, 0)
 basic.pause(1000)
 }
-let ampel_liste: number[] = []
 let ampel_nr = 0
+let ampel_liste: number[] = []
+ampel_nr = 0
 ampel_liste = [100, 103, 106, 109, 112]
 basic.forever(function () {
     eine_seite(ampel_nr)
